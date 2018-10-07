531a4b30cfe05ec7bb770ffd6d90707d15aec261
Caterina Doglioni <caterina.doglioni@cern.ch>
2018-10-07 15:43:31 +0200

    Raffaele's comments

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -ne 0 ]; then
-pdflatex 2HDMa-whitepaper
-bibtex 2HDMa-whitepaper
-pdflatex 2HDMa-whitepaper
-pdflatex 2HDMa-whitepaper
+pdflatex 2HDMa-whitepaper-external
+bibtex 2HDMa-whitepaper-external
+pdflatex 2HDMa-whitepaper-external
+pdflatex 2HDMa-whitepaper-external
 else
-pdflatex 2HDMa-whitepaper
+pdflatex 2HDMa-whitepaper-external
 fi
 
