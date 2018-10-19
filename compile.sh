8f122fbeeb8225d28a676b1eca71dcacbe1fbc9e
Caterina Doglioni <caterina.doglioni@cern.ch>
2018-10-19 08:52:27 +0200

    final version

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -ne 0 ]; then
-pdflatex 2HDMa-whitepaper-external
-bibtex 2HDMa-whitepaper-external
-pdflatex 2HDMa-whitepaper-external
-pdflatex 2HDMa-whitepaper-external
+pdflatex 2HDMa-whitepaper-final
+bibtex 2HDMa-whitepaper-final
+pdflatex 2HDMa-whitepaper-final
+pdflatex 2HDMa-whitepaper-final
 else
-pdflatex 2HDMa-whitepaper-external
+pdflatex 2HDMa-whitepaper-final
 fi
 
